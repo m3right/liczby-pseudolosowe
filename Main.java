5e7cdab3c935de21a55150df7012f72fc1e76a2b
Ania Dermin <>
2023-01-25 20:16:30 +0000

    Range of function

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.*; //gwiazdka oznacza ze importujemy wszystkie klasy
 
-public class Main {
+/*public class Main {
    public static void main(String[] args) {
       int los;
       Random rand = new Random();
@@ -11,9 +11,20 @@ public class Main {
       while (los != 50);
       System.out.println();
    }
-}
+}*/
 
 /*
  * Cwiczenia:
  * 1. Zmien gorna granice przedzialu losowania
- */
+ */public class Main {
+   public static void main(String[] args) {
+      int los;
+      Random rand = new Random();
+      do {
+         los = rand.nextInt(120);
+         System.out.print(los+", ");
+      } 
+      while (los != 50);
+      System.out.println();
+   }
+}
